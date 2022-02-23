c3599cf3c06785c325e7b7e068143911433db0ad
mohan23599km <mohan23599.km@gmail.com>
2022-02-23 13:10:16 +0530

    done

---
+++ src/org/example/Example2.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Example2 {
+	public void forabstract() {
+		System.out.println("partially abstraction");
+		
+	}
+	public void mohan() {
+		System.out.println("it is name");
+		
+	}
+	
+	public static void main(String[] args) {
+		Example2 e=new Example2();
+		e.forabstract();
+		e.mohan();
+	}
+
+}
---
+++ src/org/example/example.java
@@ -0,0 +1,28 @@
+package org.example;
+//partially abstraction
+//abstract class >abstract method> non-abstract method
+
+//abstract class
+public abstract class example extends Example2{
+	//abstract class
+	public abstract void forabstract(); 
+	public abstract void mohan();
+	
+// non abstract method is a normal method
+	public void secondName() {
+		System.out.println("kumar");
+	}
+
+	}		
+
+	
+
+
+
+
+		
+
+
+		
+
+
